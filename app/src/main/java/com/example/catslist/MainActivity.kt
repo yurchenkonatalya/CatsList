6e6bac9d15d77979b2317a950c7a3649dccd566b
yurchenko <sckrepp@gmail.com>
2022-04-19 01:17:57 +0300

    add radio button

--- app/src/main/java/com/example/catslist/MainActivity.kt
+++ app/src/main/java/com/example/catslist/MainActivity.kt
@@ -2,16 +2,21 @@ package com.example.catslist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navButtom: BottomNavigationView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navButtom = findViewById(R.id.bottom_navigation)
 
+        val navController = (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav?.let{
+            NavigationUI.setupWithNavController(it, navController)
+        }
     }
 }
